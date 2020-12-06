c27d61a38bc7fcfdd4b8f4c2c52bad890124fd2e
pakiosann@gmail.com <pakiosann@gmail.com>
2020-12-06 09:25:56 +0900

    init

---
+++ server/src/test/java/com/pakio/demoelasticsearch/DemoElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pakio.demoelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
